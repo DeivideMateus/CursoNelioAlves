94f6fbd16776f0071eb3edfaa892d57427545e4d
DeivideMateus <44448709+DeivideMateus@users.noreply.github.com>
2020-11-24 23:07:05 -0300

    OrderProject initialized

---
+++ CursoNelio/OrderProject/Entities/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProject.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        //Price = price; Recebendo o parâmetro price no construtor?
+        public OrderItem(int quantity, Product product)
+        {
+            Quantity = quantity;
+            Price = product.Price;      
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
