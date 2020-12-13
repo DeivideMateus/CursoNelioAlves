15a3a215f7d462f2f4c68782e21d53a4350632f3
DeivideMateus <44448709+DeivideMateus@users.noreply.github.com>
2020-12-13 10:28:35 -0300

    AccountExercise

---
+++ CursoNelio/AccountExercise/Entities/Exceptions/WithdrawException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountExercise.Entities.Exceptions
+{
+    class WithdrawException : Exception
+    {
+        public WithdrawException(string msg) : base(msg)
+        {
+        }
+    }
+}
