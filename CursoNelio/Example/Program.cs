78a354661a74702958e259f67eb55bf9f2901437
DeivideMateus <44448709+DeivideMateus@users.noreply.github.com>
2020-12-20 16:04:09 -0300

    Estudando Arquivos

--- CursoNelio/Example/Program.cs
+++ CursoNelio/Example/Program.cs
@@ -7,11 +7,130 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            string sourcePath = @"c:\temp\file1.txt.txt";
-            string targetPath = @"c:\temp\file2.txt.txt";
 
-            FileInfo file = new FileInfo(sourcePath);
-            file.CopyTo(targetPath);
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+
+            try
+            {
+                FileInfo file = new FileInfo(sourcePath);
+                file.CopyTo(targetPath);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            //------------------------------------------------------------------------------------
+            //Streams fechando com .Close();
+            Console.WriteLine();
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(sourcePath);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+
+            //--------------------------------------------------------------------------------------
+            //BLOCO USING (Fechar os streams sem necessidade com .Close();
+            Console.WriteLine();
+            try
+            {
+                using (sr = File.OpenText(sourcePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            //--------------------------------------------------------------------------------------
+            //StreamWriter
+            Console.WriteLine();
+            string targetPath2 = @"C:\temp\file3.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath2))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            //--------------------------------------------------------------------------------------
+            //Directory: Criando/listando pastas e listando Files.
+            Console.WriteLine();
+            string path = @"C:\temp\myfolder";
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Console.WriteLine();
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            //--------------------------------------------------------------------------------------
+            //Classe Path
+            Console.WriteLine();
+            path = @"c:\temp\myfolder\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
         }
     }
 }
