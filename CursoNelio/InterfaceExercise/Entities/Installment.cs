3492f7c75b17fd5d262ee592375c3a34d62c11ee
DeivideMateus <44448709+DeivideMateus@users.noreply.github.com>
2021-01-06 17:39:58 -0300

    att interface

--- CursoNelio/InterfaceExercise/Entities/Installment.cs
+++ CursoNelio/InterfaceExercise/Entities/Installment.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace InterfaceExercise.Entities
 {
     class Installment
     {
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime date, double amount)
+        {
+            Date = date;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return Date.ToString("dd/MM/yyyy")
+                + " - " 
+                + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
     }
 }
