b46c7f2dfbece1223a1c1cf45d61c86a718b5da7
DeivideMateus <44448709+DeivideMateus@users.noreply.github.com>
2021-01-14 16:52:08 -0300

    Generics, Sets and Dictionary

---
+++ CursoNelio/SetExercise2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> courseA = new HashSet<int>();
+            HashSet<int> courseB = new HashSet<int>();
+            HashSet<int> courseC = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int students = int.Parse(Console.ReadLine());
+            for (int i = 0; i < students; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                courseA.Add(cod);
+            }
+
+            Console.Write("How many students for course B? ");
+            students = int.Parse(Console.ReadLine());
+            for (int i = 0; i < students; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                courseB.Add(cod);
+            }
+
+            Console.Write("How many students for course C? ");
+            students = int.Parse(Console.ReadLine());
+            for (int i = 0; i < students; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                courseC.Add(cod);
+            }
+
+            HashSet<int> all = new HashSet<int>();
+            all.UnionWith(courseA);
+            all.UnionWith(courseB);
+            all.UnionWith(courseC);
+
+            Console.WriteLine("Total students: " + all.Count);
+        }
+    }
+}
---
+++ CursoNelio/SetsExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SetsExercise.Entities;
+
+namespace SetsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //usando HashSet ao invés de SortedSet, por ser mais rápido e a ordem não importar.
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
